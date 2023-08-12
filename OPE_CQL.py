c2b82d30803e2166597eff4762d1bf2a43d85c62
Ainaz Jamshidi <47214448+ajam74001@users.noreply.github.com>
2023-08-12 11:18:46 +0330

    Update OPE_CQL.py

--- OPE_CQL.py
+++ OPE_CQL.py
@@ -10,6 +10,18 @@ from d3rlpy.algos import DiscreteCQL
 from sklearn.linear_model import LogisticRegression
 from d3rlpy.models.encoders import VectorEncoderFactory
 import pickle
+import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-OldP' , type= str, default= "../lane-change/DqnPolicy-4lanes-gamma0.99-100ksteps.h5", help= 'the path to the dataset collected using the old policy')
+# parser.add_argument('-NewP' , type= str, default= "../lane-change/cql64_g0.99-4lanes-DqN/cql_model_dqndata_4lanes_seed_50_ep300gamma0.pt", help= 'the path to the new policy model')
+
+# args = parser.parse_args()
+# OldP_path = args.OldP
+# NewP_path = args.NewP
+
+
+
 def action_probs(X,y):
     # fiting a LR to the observations and predict the probs of each actions X: observation , y: actions taken by the policy 
     model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
@@ -61,26 +73,32 @@ def policy_preparation(dataset):
     obs_s = pd.Series(obs_dict)
     res = pd.concat([obs_s,pd.DataFrame(yhat)], axis = 1)
     res.columns= ['context', 'action0', 'action1', 'action2', 'action3', 'action4']
+    res['context'] = res['context'].apply(lambda x: str(x))
+    # Set 'context' as the index
+    res.set_index('context', inplace=True)
     # print(res)
     return res
 
 
 
 if __name__ == "__main__":
-    def action_probabilities(context): # This function gives the new probabilties based on the new policy and the action probs are for the expert (policy first), the library expects only one input to this function that is why i wrote it this way
-    # if context what then i will return a
-#     pass
-    # df_newP[df_newP['context']==context]
-    # return {0:df_newP.iloc[context,1], 1:df_newP.iloc[context,2], 2:df_newP.iloc[context,3], 3:df_newP[4], 4:df_newP[5]}
-        return {0: df_newP[df_newP['context']==context]['action0'], 1: df_newP[df_newP['context']==context]['action1'], 2:df_newP[df_newP['context']==context]['action2'], 3:df_newP[df_newP['context']==context]['action3'], 4:df_newP[df_newP['context']==context]['action4']}
-
+    # This function gives the new probabilties based on the new policy and the action probs are for the expert (policy first), the library expects only one input to this function that is why i wrote it this way
+    def action_probabilities(context):
+        context = str(context)
+        if context in df_newP.index:
+            return {0: df_newP.loc[context]['action0'],
+                    1: df_newP.loc[context]['action1'],
+                    2: df_newP.loc[context]['action2'],
+                    3: df_newP.loc[context]['action3'],
+                    4: df_newP.loc[context]['action4']}
+        else:
+            # Return some default value if the context is not found
+            return {0: 0.2, 1: 0.2, 2: 0.2, 3: 0.2, 4: 0.2}
+    
     dataset = MDPDataset.load("../lane-change/DqnPolicy-4lanes-gamma0.99-100ksteps.h5")
 
     df = data_preparation(dataset)
     # print(df.head())
     df_newP = policy_preparation(dataset)
-    result = doubly_robust.evaluate(df, action_probabilities, num_bootstrap_samples=50)
-    
-    with open('result.pickle', 'wb') as f: 
-        pickle.dump(result, f)
+    result = doubly_robust.evaluate(df, action_probabilities, num_bootstrap_samples=5)
     print(result)
